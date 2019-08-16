b89119c1d370d23c95696efeb650437aae2ded8c
rajat-bhagat <rajat.bhagat6897@gmail.com>
2019-08-16 03:34:29 -0400

    stack code using array

---
+++ stack.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void push();
+int pop();
+void display();
+
+int top=-1;
+int a[10]={0};
+
+int main()
+{
+	int c=0;
+	int n;
+	while(1)
+	{
+		
+		printf("\n1.Insert\n2.Display\n3.Delete\n4.Exit\n");
+		printf("Enter your choice:\n");
+		scanf("%d",&n);
+		switch(n)
+		{	
+			case 1:
+				push();
+				break;
+			case 2:
+				display();
+				break;
+			case 3:
+				pop();
+				//printf("value poped is %d",c);
+				break;
+			case 4: exit(1);
+		}
+	}
+}
+
+void push()
+{	int x=0;
+	if(top==10)
+	{
+		printf("\n Stack overflow");
+	}
+	else
+	{
+		printf("Enter value to push : ");
+		scanf("%d",&x);
+
+		top++;
+		a[top]=x;
+	}
+}
+
+int pop()
+{
+	//return a[top];
+	printf("value poped is %d\n",a[top]);
+	top--;
+}
+
+void display()
+{
+	if(top==-1)
+		printf("\n stack empty");
+	else
+	{
+		for(int i=0;i<=top;i++)
+		{
+			printf("%d ",a[i]);
+		}
+	}
+}
